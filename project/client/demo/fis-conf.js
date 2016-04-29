498bb9fee799f8bf2706c8213bf0a8a9ecd17fe2
longzhiyou <214234574@qq.com>
2016-04-29 12:52:49 +0800

    提交到git

---
+++ project/client/demo/fis-conf.js
@@ -0,0 +1,25 @@
+// fis.match('::packager', {
+//   spriter: fis.plugin('csssprites')
+// });
+
+//清除其他配置，只剩下如下配置
+fis.match('*.{js,css,png}', {
+    useHash: true
+});
+
+// fis.match('*', {
+//   useHash: false
+// });
+
+// fis.match('*.js', {
+//   optimizer: fis.plugin('uglify-js')
+// });
+
+// fis.match('*.css', {
+//   useSprite: true,
+//   optimizer: fis.plugin('clean-css')
+// });
+
+// fis.match('*.png', {
+//   optimizer: fis.plugin('png-compressor')
+// });
